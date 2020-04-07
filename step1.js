c66172d8a9a48fb16c2856bc4c885f2ef22905aa
Graham Trail <Graham@Grahams-MacBook-Pro.local>
2020-04-07 14:58:01 -0700

    Part two

--- step1.js
+++ step1.js
@@ -3,8 +3,7 @@ let path = process.argv[2]
 function cat (path){
     fs.readFile(path, 'utf8', function(err, data){
         if(err){
-            console.log(`Error:
-            ${err.message}`)
+            console.log(`Error: ${err.message}`)
             process.exit(1)
         }
         console.log(`file contents: ${data}`);
--- step2.js
+++ step2.js
@@ -1,23 +1,67 @@
-const fs = require('fs'); 
-const axios = require('axios');
+const fs = require('fs');
+const axios = require("axios"); 
 
-let path = process.argv[2]
+let source = process.argv[2];
+
+/** Attempts to read file at path,
+ * displays content if successful, error otherwise. */ 
 
 function cat (path){
     fs.readFile(path, 'utf8', function(err, data){
         if(err){
-            console.log(`Error:
-            ${err.message}`)
+            console.log(`Error: ${err.message}`)
+            process.exit(1)
         }
         console.log(`file contents: ${data}`);
     })
 }
 
-function webCat (path){
-    axios.get(path).then(function(resp) {
-        console.log(resp.data)
-    }).catch(err => console.log(err.message))
+/** Attempts to GET a URL, displays the content
+ * if successful, error otherwise. */
+
+function webCat(url) {
+  axios.get(url)
+    .then(d => {console.log(d.data)})
+    .catch(e => {console.log(`Error fetching ${url}:\n`
+                              ,e.message)});
 }
 
-path[0] != 'h' ? cat(path): webCat(path);
+/** Determines if the source is a file path or URL 
+ * and calls the relevent function.
+ */
+
+function generalCat(source) {
+  if (source.startsWith("http")) {
+    webCat(source);
+  } else {
+    cat(source);
+  }
+}
+
+generalCat(source);
+
+
+// const fs = require('fs'); 
+// const axios = require('axios');
+
+// let path = process.argv[2]
+
+// function cat (path){
+//     fs.readFile(path, 'utf8', function(err, data){
+//         if(err){
+//             console.log(`Error:
+//             ${err.message}`)
+//         }
+//         console.log(`file contents: ${data}`);
+//     })
+// }
+
+// function webCat (path){
+//     axios.get(path).then(function(resp) {
+//         console.log(resp.data)
+//     }).catch(err => console.log(err.message))
+// }
+
+// path[0] != 'h' ? cat(path): webCat(path);
     
+
